375db31277c8f79d58d97494c0d6caebcf85e09d
Vitalii Honchar <weaxme@gmail.com>
2025-09-06 13:53:03 +0200

    Improved strategy

--- src/shorts_creator/video_effect/strategies.py
+++ src/shorts_creator/video_effect/strategies.py
@@ -11,18 +11,17 @@ from typing import Sequence
 
 
 class VideoEffectsStrategy(Enum):
-    BASIC = (
-        "basic",
-        [
-            BlurFilterStartVideoEffect(blur_strength=20, duration=1.0),
-            # IncreaseVideoSpeedEffect(speed_factor=1.5, fps=30),
-            # VideoRatioConversionEffect(target_w=1080, target_h=1920),
-            # PixelateFilterStartVideoEffect(pixelation_level=20, duration=1.0),
-            # TextEffect(text="New Video", text_align="top"),
-            # TextEffect(text="SUBSCRIBE ON ME", text_align="bottom"),
-        ],
-    )
+    BASIC = ("basic",)
 
-    def __init__(self, code: str, effects: Sequence[VideoEffect]):
-        self.code = code
-        self.effects = effects
+    def create_effects(self) -> Sequence[VideoEffect]:
+        match self:
+            case VideoEffectsStrategy.BASIC:
+                return [
+                    VideoRatioConversionEffect(target_w=1080, target_h=1920),
+                    TextEffect(text="New Video", text_align="top"),
+                    TextEffect(text="SUBSCRIBE ON ME", text_align="bottom"),
+                    BlurFilterStartVideoEffect(blur_strength=20, duration=1.0),
+                    IncreaseVideoSpeedEffect(speed_factor=1.5, fps=30),
+                ]
+            case _:
+                raise ValueError(f"Unknown strategy: {self}")
